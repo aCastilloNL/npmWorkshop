6e38747df40755fe8b25c6eed27fd5433740d105
Alfred <castellum.a@gmail.com>
2020-11-02 11:01:29 +0100

    initial commit for console logging colleague names in diff colours

---
+++ index.js
@@ -0,0 +1,8 @@
+const chalk = require('chalk');
+const log = console.log;
+
+log(chalk.blue('Alfred'));
+log(chalk.red('Lennart'));
+log(chalk.red('Jane'));
+log(chalk.green('Niko'));
+log(chalk.yellow('Marc'));
